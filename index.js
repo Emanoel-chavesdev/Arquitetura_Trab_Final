2d2e8f359d21834cbfcfa33d27c0f784a1929c3c
Emanoel-chavesdev <111396501+Emanoel-chavesdev@users.noreply.github.com>
2023-06-14 20:33:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFML06blRl1x-8A8qhS-X-PuThgAsTdvk",
+  authDomain: "chatcomchatgpt.firebaseapp.com",
+  projectId: "chatcomchatgpt",
+  storageBucket: "chatcomchatgpt.appspot.com",
+  messagingSenderId: "680224458767",
+  appId: "1:680224458767:web:1674d7728146123eca7b7a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
